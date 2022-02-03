473b315e94f003fc5270593bdd44c3375487a7e6
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2022-02-02 22:25:00 -0500

    Update index.js

--- week4/js/index.js
+++ week4/js/index.js
@@ -5,17 +5,12 @@ function toggleMenu () {
 }
 const x = document.getElementById("chamberButton");
 x.onclick = toggleMenu
-//current date
-const datefield = document.querySelector("#date");
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
-datefield.innerHTML = `<em>${fulldate}</em>`;
-//current year
-document.addEventListener("DOMContentLoaded", function() { 
-    const yrSpan = document.querySelector('#currentyear');
-    const currentYr = new Date().getFullYear();
-    yrSpan.textContent = currentYr;
-  });
-//last modified date and hour
-const options = {month: "numeric", day:"numeric", year: "numeric", hour12: false, hour:"2-digit", minute:"2-digit", second:"2-digit"};
-document.getElementById("lastmodified").textContent = new Date(document.lastModified).toLocaleDateString("en-US",options);
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+var x = document.lastModified;
+document.getElementById("date").innerHTML = x;
+
+function toggleMenu() {
+   
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
