edf28cc51e31591293b11f3d362cd101c50dba59
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2022-02-28 13:07:50 -0500

    Update script.js

--- week4/js/script.js
+++ week4/js/script.js
@@ -1,11 +1,9 @@
-//chamber menu
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+var x = document.lastModified;
+document.getElementById("date").innerHTML = x;
+
 function toggleMenu() {
-    var temp = document.getElementsByClassName("toggler")[0]
-    temp.classList.toggle("hide")
-    if (temp.classList[1]=="hide") {
-        document.querySelector(".toggler > a").innerHTML = "&#9748; Menu"
-    }
-    else {
-        document.querySelector(".toggler > a").innerHTML = "&#10060; Close"
-    }
+   
+    document.getElementById("primaryNav").classList.toggle("hide");
 }
