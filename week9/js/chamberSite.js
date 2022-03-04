c83513f68e07a1dc4a491ec273d9b78ca6239131
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2022-03-04 10:09:03 -0500

    Update chamberSite.js

--- week9/js/chamberSite.js
+++ week9/js/chamberSite.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-  $('button').click(function(){
-      $('ul').toggleClass('show');
-  });
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
