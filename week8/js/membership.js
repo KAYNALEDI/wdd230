d7e187ddf77b8de9f21e081e5f721ddb5b5a0966
KAYNALEDI <72366409+KAYNALEDI@users.noreply.github.com>
2022-03-25 23:44:26 -0400

    Update membership.js

--- week8/js/membership.js
+++ week8/js/membership.js
@@ -108,3 +108,27 @@ function validateControls() {
 function placeOrder(form){
     form.submit();
 }
+
+
+const submitButton = document.getElementById("submit-button");
+const reqField = document.querySelectorAll("input[required]");
+let validForm = true;
+submitButton.addEventListener("click", () => {
+    validForm = true;
+    const isInputFilled = [];
+    reqField.forEach((field) => {
+        if (field.value === "") isInputFilled.push(false)
+        else isInputFilled.push(true)
+    })
+    for (let i = 0; i < isInputFilled.length; i++){
+        if (!isInputFilled[i]) {
+            validForm = false;
+            break
+        }
+    }
+    if (validForm) window.location.href = "thankyou.html";
+    else {
+        alert("Please fill all required fields.");
+        reqField.forEach((field) => field.classList.add("required-highlight"));
+    }
+})
